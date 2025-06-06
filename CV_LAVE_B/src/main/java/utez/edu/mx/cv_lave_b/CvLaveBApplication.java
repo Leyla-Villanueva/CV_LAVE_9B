d247ff245f19cde7146b16298b3461a49bc19ee7
Leyla-Villanueva <leylavillanueva0205@gmail.com>
2025-06-06 10:43:00 -0600

    [FIST COMMIT] Subida inicial del proyecto

---
+++ CV_LAVE_B/src/main/java/utez/edu/mx/cv_lave_b/CvLaveBApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_lave_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvLaveBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvLaveBApplication.class, args);
+    }
+
+}
---
+++ CV_LAVE_B/src/test/java/utez/edu/mx/cv_lave_b/CvLaveBApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_lave_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvLaveBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
